9bcd99d70c44b2c24b683e511c9860446b938f47
okjm3324 <yuma.okjm@gmail.com>
2022-08-25 22:48:39 +0900

    revised config

--- next.config.js
+++ next.config.js
@@ -14,12 +14,13 @@ module.exports = () => {
     if (process.env.NODE_ENV === 'production'){
       return [
         {
-          source: `${process.env.API_BASE_URL}/api/posts`,
-          destination: "http://localhost:5000/api/posts",
+          source: "/posts",
+          destination: `${process.env.API_BASE_URL}/api/posts`
         }
       ]
 
     }else{
+      console.log("開発環境です")
       return [
         {
           source: "/posts",
